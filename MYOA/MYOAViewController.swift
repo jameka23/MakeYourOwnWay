f1ed1a26e45b6ba434a1a3f60c3213688e9fa96c
Jameka Echols <jameka.echols@gmail.com>
2021-06-25 22:42:08 -0500

    added the wrong bar button to navigation.

--- MYOA/MYOAViewController.swift
+++ MYOA/MYOAViewController.swift
@@ -13,13 +13,17 @@ class MYOAViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        navigationItem.backBarButtonItem = UIBarButtonItem(title: "Start Over", style: .plain, target: self, action: #selector(startOver))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Start Over", style: .plain, target: self, action: #selector(startOver))
     }
     
     @objc func startOver(){
         if let navigationController = navigationController {
             navigationController.popToRootViewController(animated: true)
         }
+    }
+    
+    deinit {
+        print("deallocation just happened. Boom!")
     }
 
 }
